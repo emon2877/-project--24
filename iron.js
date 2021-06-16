1bc563661beff90d5b62a0f2e2b4074c87386db8
emon2877 <80687601+emon2877@users.noreply.github.com>
2021-06-16 12:55:19 +0530

    Add files via upload

---
+++ iron.js
@@ -0,0 +1,28 @@
+class iron {
+    constructor(x, y) {
+      var options = {
+        'density':0.8,
+        'friction': 3,
+        'restitution':1
+      };
+      this.body = Bodies.rectangle(x, y, 50, 50, options);
+      this.width = 170;
+      this.height = 130;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke('pink');
+      fill('blue');
+      rectMode(CENTER)
+      rect(0, 0, this.width, this.height);
+      pop();
+    };
+  };
+  
